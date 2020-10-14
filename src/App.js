21a26b2b61621fd821b76f282afdec3cbb7c502f
sourav <souravghosh1500@gmnail.com>
2020-10-14 09:04:56 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {
+  useEffect,
+  createContext,
+  useReducer,
+  userContext,
+  useContext,
+} from "react";
+import "./App.css";
+import Navbar from "./components/Screens/Navbar";
+import Signin from "./components/Screens/Signin";
+import Home from "./components/Screens/Home";
+import { BrowserRouter, Route, Switch, useHistory } from "react-router-dom";
+import UserProfile from "./components/Screens/Userprofile";
+import Profile from "./components/Screens/Profile";
+import Signup from "./components/Screens/Signup";
+import Createpost from "./components/Screens/Createpost";
+import Subpost from "./components/Screens/Subscribepost";
+import { reducer, initialState } from "./reducers/userReducer";
 
-function App() {
+export const UserContext = createContext();
+
+const Routing = () => {
+  const history = useHistory();
+  const { state, dispatch } = useContext(UserContext);
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (user) {
+      dispatch({ type: "USER", payload: user });
+    } else {
+      history.push("/signin");
+    }
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/signin">
+        <Signin />
+      </Route>
+      <Route exact path="/signup">
+        <Signup />
+      </Route>
+      <Route exact path="/profile">
+        <Profile />
+      </Route>
+      <Route exact path="/create">
+        <Createpost />
+      </Route>
+      <Route exact path="/profile/:id">
+       <UserProfile />
+      </Route>
+      <Route exact path="/subpost">
+       <Subpost />
+      </Route>
+    </Switch>
+  );
+};
+function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  return (
+    <UserContext.Provider value={{ state, dispatch }}>
+      <BrowserRouter>
+        <Navbar />
+        
+        <Routing />
+      </BrowserRouter>
+    </UserContext.Provider>
   );
 }
 
